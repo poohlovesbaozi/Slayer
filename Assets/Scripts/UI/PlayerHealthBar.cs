57cd24db47a36fa829d5d9b237b32124126b0ccf
poohlovesbaozi <libertassss@hotmail.com>
2023-09-03 23:39:12 +0800

    修改了血条不随玩家localscale变动

--- Assets/Scripts/UI/PlayerHealthBar.cs
+++ Assets/Scripts/UI/PlayerHealthBar.cs
@@ -7,6 +7,10 @@ using UnityEngine.UI;
 public class PlayerHealthBar : MonoBehaviour
 {
     [SerializeField] Image greenHealthImage;
+    [SerializeField] Transform playerTrans;
+    private void Update() {
+            greenHealthImage.fillOrigin=-(int)playerTrans.localScale.x;
+    }
 
     /// <summary>
     /// accept hp percentage to change the health bar image.
